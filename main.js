d0c3b791a6b12a6e7503eeef56cf0a789d4adcd7
Diana Ahn <misspinkade@gmail.com>
2019-11-05 21:24:08 -0800

    Create HTML, CSS, and Javascript File

---
+++ main.js
@@ -0,0 +1,8 @@
+alert("Open the console!");
+
+for (let count = 1; count <= 5; count++) {
+    console.log("*".repeat(count));
+}
+for (let count = 4; count >= 1; count--) {
+    console.log("*".repeat(count));
+}
